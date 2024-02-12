3adf054efd2909d231762bdb2d5bc35e59ab740e
Deda <marko.barisic@memgraph.io>
2024-02-12 19:40:03 +0100

    Removed container checking, added more verbose requirement checking

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 OS=$(uname -s)
 ARCH=$(uname -m)
 
+CHECK_UNICODE="\xE2\x9C\x94"
+FAIL_UNICODE="\033[1mx\033[0m"
+
 if [ "${OS}" = "Linux" ]
 then
   CMD_PREFIX="/bin/"
@@ -14,54 +17,51 @@ else
 fi
 
 msg_out() {
-  ${CMD_PREFIX}echo -e "$1\n"
+  ${CMD_PREFIX}echo -e "$1"
 }
 
 err_out() {
-  msg_out "Error: $1" >&2 && exit 1
+  msg_out "$(bold "ERROR"): $1" >&2
+  exit 1
+}
+
+bold() {
+  msg_out "\033[1m${1}\033[0m"
 }
 
 check_cmd_dep() {
-  if [ "$2" = "--err" ]; then
-    command -v ${CMD_PREFIX}$1 >/dev/null 2>&1 || err_out "you need to have '$1' installed"
-  elif [ "$2" = "--no-err" ]; then
-    command -v ${CMD_PREFIX}$1 >/dev/null 2>&1
+  local check_cmd="command -V ${CMD_PREFIX}$1"
+  if [ "$1" = "docker compose" ]; then
+    check_cmd="${CMD_PREFIX}docker compose version"
+  fi
+  if $check_cmd > /dev/null 2>&1; then
+    msg_out "$1 $CHECK_UNICODE"
+    return 0
+  else
+    msg_out "$1 $(bold "x")"
+    $check_cmd
+    return 1
   fi
 }
 
-check_running_container() {
-  ${CMD_PREFIX}docker container inspect $1 > /dev/null 2>&1 && \
-  msg_out "Container $1 is already running on this machine"
-}
-
-check_port_availability() {
-  ${CMD_PREFIX}lsof -i | ${CMD_PREFIX}grep LISTEN | ${CMD_PREFIX}grep :$1 > /dev/null 2>&1 && \
-  msg_out "Another process on this machine is already using port $1"
-}
-
-# Check for required commands
-check_cmd_dep "docker" --err
-check_cmd_dep "grep" --err
-check_cmd_dep "lsof" --err
-check_cmd_dep "mkdir" --err
-check_cmd_dep "pwd" --err
-
-# Check for curl or wget commands
-if check_cmd_dep "curl" --no-err; then
-    _download_cmd=curl
-elif check_cmd_dep "wget" --no-err; then
-    _download_cmd=wget
+# Check required commands
+msg_out "$(bold "Checking for requirements on this machine:")"
+check_deps=1
+if check_cmd_dep "curl"; then
+  _download_cmd=curl
+elif check_cmd_dep "wget"; then
+  _download_cmd=wget
 else
-    err_out "you need to have 'curl' or 'wget' installed"
+  check_deps=0
+  eerr_out "you need to have 'curl' or 'wget' installed"
 fi
-
-# Check for docker compose or docker-compose
-if ${CMD_PREFIX}docker compose version >/dev/null 2>&1; then
-  _compose_cmd="docker compose"
-elif check_cmd_dep "docker-compose"; then
-  _compose_cmd="docker-compose"
-else
-  err_out "you need to have 'docker compose' or 'docker-compose' installed"
+check_cmd_dep "docker" || check_deps=0
+check_cmd_dep "docker compose" || check_deps=0
+check_cmd_dep "grep" || check_deps=0
+check_cmd_dep "mkdir" || check_deps=0
+check_cmd_dep "pwd" || check_deps=0
+if [ "$check_deps" -eq 0 ]; then
+  err_out "All requirements must be satisfied to run this script!"
 fi
 
 DIR=$(${CMD_PREFIX}pwd)
@@ -71,24 +71,17 @@ DOCKER_COMPOSE_URL="https://raw.githubusercontent.com/memgraph/memgraph-platform
 
 # Check if compose file already exists
 if [ -f "$MGPLAT_COMPOSE_PATH" ]; then
-  msg_out "Overwriting docker compose file found at $MGPLAT_COMPOSE_PATH"
+  msg_out "\n$(bold "Overwriting docker compose file found at:") $MGPLAT_COMPOSE_PATH"
 elif [ ! -d $MGPLAT_DIR ]; then
   ${CMD_PREFIX}mkdir -p $MGPLAT_DIR
 fi
 
-msg_out "Downloading docker compose file to $MGPLAT_COMPOSE_PATH"
-${CMD_PREFIX}$_download_cmd $DOCKER_COMPOSE_URL -o "$MGPLAT_DIR/docker-compose.yml" > /dev/null 2>&1 || \
-err_out "something went wrong when dowloading docker-compose.yml from $DOCKER_COMPOSE_URL"
-
-# Check if containers with the same names are already running
-check_running_container "memgraph-lab"
-check_running_container "memgraph-mage"
-
-# Check if ports are in use
-# check_port_availability 3000
-# check_port_availability 7687
-# check_port_availability 7444
+# Download compose file
+msg_out "\n$(bold "Downloading docker compose file to:") $MGPLAT_COMPOSE_PATH"
+${CMD_PREFIX}$_download_cmd $DOCKER_COMPOSE_URL -o "$MGPLAT_DIR/docker-compose.yml" || \
+err_out "Something went wrong when dowloading docker-compose.yml from $DOCKER_COMPOSE_URL"
 
 # Run compose
 cd $MGPLAT_DIR
+msg_out "\n$(bold "Spinning up memgraph lab and memgraph with mage using docker compose file from:") $MGPLAT_COMPOSE_PATH"
 ${CMD_PREFIX}docker compose up
